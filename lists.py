e58b809833f79395d19702b1053af69f12a4396b
Peter Emu <emu.kimani@strathmore.edu>
2025-11-14 22:12:54 +0300

    list complete

--- lists.py
+++ lists.py
@@ -29,6 +29,49 @@ def subtract ():
 
 print(subtract())
     
+    #question 3
+sample_list = ['p', 'q']
+n = 5
+
+result = [item + str(i) for i in range(1, n+1) for item in sample_list]
+
+print(result)
+
+#question 4
+colors = ["Black", "Red", "Maroon", "Yellow"]
+codes = ["#000000", "#FF0000", "#800000", "#FFFF00"]
+
+result = [{'color_name': c, 'color_code': d} for c, d in zip(colors, codes)]
+
+print(result)
+ 
+#question 5
+nums = [3, 4, 0, 0, 0, 6, 2, 0, 6, 7, 6, 0, 0, 0, 9, 10, 7, 4, 4, 5, 3, 0, 0, 2, 9, 7, 1]
+
+result = [x for x in nums if x != 0] + [0] * nums.count(0)
+
+print("Original:", nums)
+print("After  zeros:", result)
+
+#question 6
+nums = [22.4, 4.0, 16.22, 9.1, 11.0, 12.22, 14.2, 5.2, 17.5]
+
+rounded = [round(x) for x in nums]
+print("Minimum value:", min(rounded))
+print("Maximum value:", max(rounded))
+
+multiplied = sorted(set([x * 5 for x in rounded]))
+
+print("Result:")
+print(*multiplied)
+
+# question 7
+lst = [[1, 3], [5, 7], [9, 11], [13, 15, 17]]
+
+count = sum(1 for item in lst if isinstance(item, list))
+
+print("The number of lists:", count)
+
 
 
 
